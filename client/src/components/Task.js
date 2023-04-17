f9c47c4b2fea4c4474a955336fd7e92398e10491
Jacob Park <jacobypark122@gmail.com>
2023-04-17 12:25:23 -0400

    added Task card

---
+++ client/src/components/Task.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Task = () => {
+
+    return (
+        <div className = "task-card">
+            <h2>Task</h2>
+            {/* Task details */}
+        </div>
+    );
+};
