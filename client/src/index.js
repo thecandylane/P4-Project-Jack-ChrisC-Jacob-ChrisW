7ab093444ffc165a08f3ca723a1bcd4c08f02d32
BeaconBacon <62158062+BeaconBacon@users.noreply.github.com>
2023-04-18 19:48:27 -0400

    frontend save point

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./components/App";
-import Auth0ProviderWithHistory from "./components/AuthProvider";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './App';
+import { ContextProvider } from './contexts/ContextProvider'; 
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Auth0ProviderWithHistory>
+    <ContextProvider>
         <App />
-      </Auth0ProviderWithHistory>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+    </ContextProvider>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
