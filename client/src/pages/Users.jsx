e2741f6b53cd179d9bcdf9074f046bbce3def27c
BeaconBacon <62158062+BeaconBacon@users.noreply.github.com>
2023-04-20 17:21:24 -0400

    random

--- client/src/pages/Users.jsx
+++ client/src/pages/Users.jsx
@@ -6,6 +6,7 @@ import { employeesData, employeesGrid} from '../data/dummy';
 import { Header } from '../components';
 
 const Users = () => {
+
   return (
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header category = "Page" title="Employees" />
