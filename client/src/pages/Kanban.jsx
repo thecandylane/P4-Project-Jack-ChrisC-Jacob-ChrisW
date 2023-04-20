224c4fa20ec40fe5582de22584b41785c6129de9
Jacob Park <jacobypark122@gmail.com>
2023-04-20 15:34:59 -0400

    kanban stuff

--- client/src/pages/Kanban.jsx
+++ client/src/pages/Kanban.jsx
@@ -26,9 +26,9 @@ const Kanban = () => {
       <Header category="App" title="Kanban" />
       <KanbanComponent
         id="kanban"
-        dataSource={kanbanData}
-        cardSettings={{ contentField: 'Summary', headerField: 'Id' }}
-        keyField="Status"
+        dataSource={taskItem}
+        cardSettings={{ contentField: 'description', headerField: 'title' }}
+        keyField="status"
         
       
       >
