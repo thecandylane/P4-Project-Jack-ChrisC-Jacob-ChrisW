d65e24a949c2da73f77761aae660eba433b51485
Jacob Park <jacobypark122@gmail.com>
2023-04-20 15:34:59 -0400

    kanban stuff

--- client/src/pages/Kanban.jsx
+++ client/src/pages/Kanban.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {KanbanComponent, ColumnsDirective, ColumnDirective } from '@syncfusion/ej2-react-kanban'
 import { kanbanData, kanbanGrid } from '../data/dummy'
 import { Header } from '../components'
@@ -6,14 +6,22 @@ import { Header } from '../components'
 
 
 const Kanban = () => {
+  const [taskItem, setTaskItem]= useState([{}]);
+
+  useEffect(()=>{
+    fetch('http://127.0.0.1:5555/tasks')
+    .then(res=>res.json()
+    .then(data=>setTaskItem(data)))
+  },[])
+
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="App" title="Kanban" />
       <KanbanComponent
         id="kanban"
-        dataSource={kanbanData}
-        cardSettings={{ contentField: 'Summary', headerField: 'Id' }}
-        keyField="Status"
+        dataSource={taskItem}
+        cardSettings={{ contentField: 'description', headerField: 'title' }}
+        keyField="status"
         
       
       >
