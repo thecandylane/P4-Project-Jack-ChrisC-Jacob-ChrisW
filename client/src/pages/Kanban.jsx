579d1a6991e0eee3e3350d2dfb65e7c31a2b12b0
Jacob Park <jacobypark122@gmail.com>
2023-04-20 20:45:33 -0400

    linked kanban board to back end

--- client/src/pages/Kanban.jsx
+++ client/src/pages/Kanban.jsx
@@ -17,6 +17,7 @@ const Kanban = () => {
       if (r.ok){
         r.json().then(data => {
           console.log(data)
+          console.log(data.tasks)
           setTaskItem(data.tasks)
         })
     } else{
@@ -30,9 +31,9 @@ const Kanban = () => {
       <Header category="App" title="Kanban" />
       <KanbanComponent
         id="kanban"
-        dataSource={kanbanData} 
-        cardSettings={{ contentField: 'Summary', headerField: 'Id' }}
-        keyField="Status"
+        dataSource={taskItem} 
+        cardSettings={{ contentField: 'description', headerField: 'title' }}
+        keyField="status"
         
       
       >
