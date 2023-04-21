abe17cbaca4b55e5b985eb7c1c062c32656d5f6d
BeaconBacon <62158062+BeaconBacon@users.noreply.github.com>
2023-04-21 07:01:29 -0400

    so far so good

--- client/src/pages/Projectview.jsx
+++ client/src/pages/Projectview.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { BsCurrencyDollar } from 'react-icons/bs';
 import { GoPrimitiveDot } from 'react-icons/go';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { Stacked, Pie, Button, SparkLine } from '../components';
+import { BiTask } from 'react-icons/bi'
 
 import { earningData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
@@ -10,6 +11,7 @@ import { useStateContext } from '../contexts/ContextProvider';
 const Projectview = ({projectId}) => {
   const [projectViewItem, setProjectViewItem] = useState({name: ""})
   const [tasks, setTasks] = useState([{id: ''}])
+  const navigate = useNavigate();
   
 
 useEffect(()=>{
@@ -50,52 +52,52 @@ const { currentColor } = useStateContext();
            h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
             <div className="flex justify-between items-center">
               <div>
-                <p className="font-bold text-gray-400">Project</p>
-                <p className= "text-2xl font-bold">{projectViewItem.name}</p>
+                <p className="font-bold text-white">Project</p>
+                <p className= "text-2xl text-white font-bold">{projectViewItem.name}</p>
 
               </div>
 
             </div>
             <div className="mt-6">
-              <Button 
-              color="white"
-              bgColor={currentColor}
-              text="Project List"
-              borderRadius="10px"
-              size="md"
-
-              />
+              <button type="button" onclick={()=> navigate('/projects')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2" >ProjectList</button>
+              <button type="button" onclick={()=> navigate('/projects')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >Task List</button>
 
 
             </div>
 
         </div>
         <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
-          {earningData.map((item) => (
+          {tasks.map((item) => (
             <div
             key={item.title}
             className="bg-white
             dark:text-gray-200
             dark:bg-secondary-dark-bg
             md:w-56
-            p-4 pt-9 rounded-2xl"
+            p-4 pt-9 rounded-2xl
+            border"
             >
-              <button type="button" style={{color:item.iconColor, backgroundColor: item.iconBg}}
-              className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl">
-              {item.icon}
+              <button type="button" 
+              className="bg-[#E5FAFB] text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl">
+                <BiTask/>
 
 
               </button>
               <p className='mt-3'>
                 <span className="text-lg font-semibold">
-                  {item.amount}
+                  {item.description}
                 </span>
-                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                {/* <span className={`text-sm text-red ml-2`}>
                   {item.percentage}
-                </span>
+                </span> */}
 
               </p>
-              <p className="text-sm text-gray-400 mt-1">{item.title}</p>
+              <div className="flex flex-row">
+                <p className="text-sm text-gray-400 mt-1 mr-4">{item.title}</p>
+                <p className="text-sm text-gray-400 mt-1">User</p>
+
+              </div>
+              
 
             </div>
 
@@ -105,92 +107,7 @@ const { currentColor } = useStateContext();
 
       </div>
 
-      <div className='flex gap-10 flex-wrap justify-center'>
-        <div className="bg-white dark:text-gray-200
-        dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
-          <div className="flex justify-between">
-            <p className="font-semibold text-xl">Task List</p>
-            <div className="flex items-center gap-4">
-              {tasks.map((fuck) => (
-                <div
-                key={fuck.id}
-                className="bg-white
-                dark:text-gray-200
-                dark:bg-secondary-dark-bg
-                md:w-56
-                p-4 pt-9 rounded-2xl"
-                >
-                {fuck.title}  </div>
-              ))}
-              {/* <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
-                <span><GoPrimitiveDot /></span>
-                <span>Expense</span>
-              </p>
-              <p className="flex items-center gap-2 text-green-200 hover:drop-shadow-xl">
-                <span><GoPrimitiveDot /></span>
-                <span>Budget</span>
-              </p> */}
 
-            </div>
-
-          </div>
-          <div className="mt-10 flex gap flex-wrap justify-center">
-            <div className="border-r-1 border-color m-4 pr-10">
-              <div>
-                <p className="text-3xl font-semibold">
-                  {/* {projectViewItem.tasks} */}
-                
-                  {/* <span className="text-3xl font-semibold">$93,438</span>
-                  <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">23%</span> */}
-                </p>
-                {/* <p className="text-gray-500 mt-1">Budget</p>
-
-              </div>
-              <div className="mt-8">
-                <p>
-                  <span className="text-3xl font-semibold">$48,438</span>
-                  
-                </p>
-                <p className="text-gray-500 mt-1">Expense</p> */}
-
-              </div>
-              <div className='mt-5'>
-                {/* <SparkLine
-                  currentColor ={currentColor}
-                  id="line-sparkline"
-                  type="Line"
-                  height="80px"
-                  width="250px"
-                  data={SparklineAreaData}
-                  color={currentColor}
-
-
-                /> */}
-
-              </div>
-              <div className="mt-10">
-                <Button 
-                  color="white"
-                  bgColor={currentColor}
-                  text="Task List"
-                  borderRadius = "10px"
-                />
-
-              </div>
-
-            </div>
-            {/* <div>
-              <Stacked width="320px"
-              height= "360px"/>
-
-            </div> */}
-
-          </div>
-
-
-        </div>
-
-      </div>
 
     </div>
   )
--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -57,22 +57,56 @@ const TaskForm = () => {
 
 
     return (
-        <form onSubmit={(e) => handleAddTask(e)} >
-            <input style={{ 'padding': '5px' }} type='text' name='title' onChange={handleChange} placeholder='Task title'></input>
-            <input style={{ 'padding': '5px' }} type='text' name='description' onChange={handleChange} placeholder='Task description'></input>
-            <select style={{ 'padding': '5px' }} name="status" defaultValue='' onChange={handleChange}>
-                <option value="" disabled >Select Status</option>
-                <option value="todo">To-Do</option>
-                <option value="inprogress">In Progress</option>
-                <option value="testing">Testing</option>
-                <option value="done">Done</option>
-            </select>
-            <input style={{ 'width': '90px', 'padding': '5px' }} type="number" name="priority" min="1" max="10" placeholder='Priority' onChange={handleChange}></input>
-            <input style={{ 'padding': '5px' }} type='date' name='due_date' onChange={handleChange} placeholder='Task due date'></input>
-            <input style={{ 'padding': '5px' }} type='number' name='user_id' onChange={handleChange} placeholder='User ID'></input>
-            <input style={{ 'padding': '5px' }} type='number' name='project_id' onChange={handleChange} placeholder='Project ID'></input>
-            <input style={{ 'padding': '5px' }} type='submit' value='Add Task' />
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+        <form className='max-w-[400px] w-full mx-auto rounded-lg bg-[#0f172a] p-8 px-8' onSubmit={(e) => handleAddTask(e)} >
+            <h2 className='text-4xl text-white font-bold text-center'>TASK FORM</h2>
+            <div className='flex flex-col text-gray-400 py-2'>
+                <input style={{ 'padding': '5px' }} type='text' name='title' onChange={handleChange} placeholder='Task title'></input>
+            </div>
+            <div className='flex flex-col text-gray-400 py-2'>
+                <input style={{ 'padding': '5px' }} type='text' name='description' onChange={handleChange} placeholder='Task description'></input>
+            </div>
+            <div className='flex flex-col text-gray-400 py-2'>
+
+            </div>
+            <div className='flex flex-col text-gray-400 py-2'>
+                <input style={{ 'padding': '5px' }} type='date' name='due_date' onChange={handleChange} placeholder='Task due date'></input>
+
+            </div>
+            <div className='flex flex-col text-gray-400 py-2'>
+                <input style={{ 'padding': '5px' }} type='number' name='user_id' onChange={handleChange} placeholder='User ID'></input>
+
+            </div>
+            <div className='flex flex-col text-gray-400 py-2'>
+                <input style={{ 'padding': '5px' }} type='number' name='project_id' onChange={handleChange} placeholder='Project ID'></input>
+
+            </div>
+            <div className=' flex flex-row text-gray-400 py-2'>
+                <div>
+                <select style={{ 'padding': '5px' }} name="status" defaultValue='' onChange={handleChange}>
+                    <option value="" disabled >Select Status</option>
+                    <option value="todo">To-Do</option>
+                    <option value="inprogress">In Progress</option>
+                    <option value="testing">Testing</option>
+                    <option value="done">Done</option>
+                </select>
+
+                </div>
+                <div className='px-10'>
+                    <input style={{ 'width': '90px', 'padding': '5px' }} type="number" name="priority" min="1" max="10" placeholder='Priority' onChange={handleChange}></input>
+
+                </div>
+
+                
+            </div>
+            <div>
+            <input className='w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white text-xs font-semibold rounded-lg' type='submit' value='Add Task' />
+               
+                
+            </div>
+            
         </form>
+    </div>
     )
 }
 
