96cac476542ca695d45c95c17720410b1a03e4ad
BeaconBacon <62158062+BeaconBacon@users.noreply.github.com>
2023-04-21 12:05:43 -0400

    stuff

--- client/src/pages/Userview.jsx
+++ client/src/pages/Userview.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { BsCurrencyDollar } from 'react-icons/bs';
 import { GoPrimitiveDot } from 'react-icons/go';
+import {KanbanComponent, ColumnsDirective, ColumnDirective } from '@syncfusion/ej2-react-kanban'
+import { kanbanData, kanbanGrid } from '../data/dummy'
 
 import { Stacked, Pie, Button, SparkLine } from '../components';
 
@@ -14,24 +16,24 @@ const Userview = ({userId}) => {
   const [projects, setProjects] = useState([{id: ''}])
   const navigate = useNavigate()
 
-  var content = $(".content p").text();
+  // var content = $(".content p").text();
 
-    if (content == "To do") {
+  //   if (content == "To do") {
 
-        $(this).css("color", "green");
-    }
-   if (content == "In progress") {
+  //       $(this).css("color", "green");
+  //   }
+  //  if (content == "In progress") {
 
-        $(this).css("color", "yellow");
-    }
-   if (content == "testing") {
+  //       $(this).css("color", "yellow");
+  //   }
+  //  if (content == "testing") {
 
-        $(this).css("color", "orange");
-    }
-    if (content == "Done") {
+  //       $(this).css("color", "orange");
+  //   }
+  //   if (content == "Done") {
 
-        $(this).css("color", "red");
-    }
+  //       $(this).css("color", "red");
+  //   }
 
   useEffect(()=>{
     fetch(`http://localhost:5555/users/${userId}`)
@@ -107,71 +109,22 @@ const { currentColor } = useStateContext();
       <div className='flex gap-10 flex-wrap justify-center'>
         <div className="bg-white dark:text-gray-200
         dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
-          <div className="flex justify-between">
-            <p className="font-semibold text-xl">Revenue Updates</p>
-            <div className="flex items-center gap-4">
-              <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
-                <span><GoPrimitiveDot /></span>
-                <span>Expense</span>
-              </p>
-              <p className="flex items-center gap-2 text-green-200 hover:drop-shadow-xl">
-                <span><GoPrimitiveDot /></span>
-                <span>Budget</span>
-              </p>
-
-            </div>
-
-          </div>
-          <div className="mt-10 flex gap flex-wrap justify-center">
-            <div className="border-r-1 border-color m-4 pr-10">
-              <div>
-                <p>
-                  <span className="text-3xl font-semibold">$93,438</span>
-                  <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">23%</span>
-                </p>
-                <p className="text-gray-500 mt-1">Budget</p>
-
-              </div>
-              <div className="mt-8">
-                <p>
-                  <span className="text-3xl font-semibold">$48,438</span>
-                  
-                </p>
-                <p className="text-gray-500 mt-1">Expense</p>
-
-              </div>
-              <div className='mt-5'>
-                <SparkLine
-                  currentColor ={currentColor}
-                  id="line-sparkline"
-                  type="Line"
-                  height="80px"
-                  width="250px"
-                  data={SparklineAreaData}
-                  color={currentColor}
+           <KanbanComponent
+              id="kanban"
+              dataSource={tasks} 
+              cardSettings={{ contentField: 'description', headerField: 'title' }}
+              keyField="status"
+              enablePersistence
+        
+      
+      >
+            <ColumnsDirective>
+              {kanbanGrid.map((item, index) =>
+              <ColumnDirective key={index} {...item} />)}
+            </ColumnsDirective>
+          </KanbanComponent>
 
 
-                />
-
-              </div>
-              <div className="mt-10">
-                <Button 
-                  color="white"
-                  bgColor={currentColor}
-                  text="Download Report"
-                  borderRadius = "10px"
-                />
-
-              </div>
-
-            </div>
-            <div>
-              <Stacked width="320px"
-              height= "360px"/>
-
-            </div>
-
-          </div>
 
 
         </div>
