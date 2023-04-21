3c971c9e55ae83f0f1c8cfe9109f4a38bb12a666
Jacob Park <jacobypark122@gmail.com>
2023-04-21 12:52:38 -0400

    working with home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -85,53 +85,37 @@ const Home = () => {
         <div className="bg-white dark:text-gray-200
         dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
           <div className="flex justify-between">
-            <p className="font-semibold text-xl">Revenue Updates</p>
-            <div className="flex items-center gap-4">
-              <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
-                <span><GoPrimitiveDot /></span>
-                <span>Expense</span>
-              </p>
-              <p className="flex items-center gap-2 text-green-200 hover:drop-shadow-xl">
-                <span><GoPrimitiveDot /></span>
-                <span>Budget</span>
-              </p>
-
-            </div>
+            <p className="font-semibold text-xl">Tasks</p>
 
           </div>
           <div className="mt-10 flex gap flex-wrap justify-center">
-            <div className="border-r-1 border-color m-4 pr-10">
-              <div>
-                <p>
-                  <span className="text-3xl font-semibold">$93,438</span>
-                  <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">23%</span>
-                </p>
-                <p className="text-gray-500 mt-1">Budget</p>
+            
+            {currentUser.tasks.map((item) => (
+              <div
+              key={item.id}
+              className="bg-white
+              dark:text-gray-200
+              dark:bg-secondary-dark-bg
+              md:w-56
+              p-4 pt-9 rounded-2xl"
+              >
+                <div>
+                  <button type="button"className="bg-white border text-5xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl mr-10">
+                    <BiTask/>
+
+
+                  </button>
+                </div>
+
+                <p className="text-sm text-black mt-1">{item.title}</p>
 
               </div>
-              <div className="mt-8">
-                <p>
-                  <span className="text-3xl font-semibold">$48,438</span>
-                  
-                </p>
-                <p className="text-gray-500 mt-1">Expense</p>
 
-              </div>
-              <div className='mt-5'>
+            ))}
+              
               
 
-              </div>
-              <div className="mt-10">
-                <Button 
-                  color="white"
-                  bgColor={currentColor}
-                  text="Download Report"
-                  borderRadius = "10px"
-                />
-
-              </div>
-
-            </div>
+          
             <div>
              
 
